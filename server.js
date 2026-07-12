5c0ba4c0b40f6384459912399d60891dcf1c0582
marcoshenriquemhbc <marcoshenriquemhbc@gmail.com>
2026-07-11 21:16:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("OK");
+});
+
 app.get("/status", (req, res) => {
     res.json({
         ok: true,
